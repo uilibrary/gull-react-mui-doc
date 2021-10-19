e097ef3d875d34736c74a2042ed5df20080ae2ca
jamir45 <jamirhossain008@gmail.com>
2021-10-19 18:41:59 +0600

    gull mui doc have created

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,6 @@
 export default {
   github: 'https://github.com/naime-hossain/gull-mui-typescript',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
+  docsRepositoryBase: 'https://github.com/uilibrary/gull-react-mui-doc.git',
   titleSuffix: ' – Gull',
   logo: (
     <>
@@ -24,7 +24,7 @@ export default {
       <meta name="twitter:url" content="https://nextra.vercel.app" />
       <meta name="og:title" content="Nextra: Next.js static site generator" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="Gull" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
